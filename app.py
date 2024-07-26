062024077f8c7859a391b86d6a60a9eec425fc9b
endurablec <leah.jeremiah@moestudents.edu.kn>
2024-07-26 14:23:25 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,3 @@ st.title("Welcome to My First Streamlit App")
 name = st.text_input("Enter your name:")
 if name:
     st.write(f"Hello, {name}! Welcome to the app.")
-!streamlit run app.py &>/content/logs.txt &
